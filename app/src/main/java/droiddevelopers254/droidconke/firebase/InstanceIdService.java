ce23e91790e42017d5065405e9a9eae5ed6b3fc7
wangerekaharun <harunwangereka@gmail.com>
2018-05-16 23:34:03 +0300

    added crashlytics,firebase remote configs and defaults,firebase cloud messaging

---
+++ app/src/main/java/droiddevelopers254/droidconke/firebase/InstanceIdService.java
@@ -0,0 +1,8 @@
+package droiddevelopers254.droidconke.firebase;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+
+
+}
