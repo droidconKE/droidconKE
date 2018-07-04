cd099eb4e5941405704b363ab255d1ffab922fdf
wangerekaharun <harunwangereka@gmail.com>
2018-07-04 11:42:03 +0300

    get session details from firestore in cloud functions complete

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ exports.sendStarredSessionNotification = functions.firestore
     .document('/starred_sessions/{userId}')
     .onCreate((snap, context) => {
     
-    // Grab the current value of what was written to the Realtime Database.
+    // Grab the current value of what was written to the Firestore.
     var data = snap.data();
     //get the start session event
     var day = data.day;
@@ -18,6 +18,9 @@ exports.sendStarredSessionNotification = functions.firestore
     var user_id =   data.user_id;
     console.log('user id is ' +  user_id);
 
+    var documentId= data.documentId;
+    console.log('documentId is '+ documentId);
+
     //device id token get it here
     admin.firestore().collection('users').doc(user_id).get().then(user => {
     if (!user.exists) {
@@ -26,27 +29,30 @@ exports.sendStarredSessionNotification = functions.firestore
         var deviceId= user.data().refresh_token;
         console.log('refresh_token:', user.data().refresh_token);
     }
+    })
+    .catch(err =>{
+        console.log('Error getting user',err);
     });
 
     //get session details
-    admin.firestore().collection(day).doc(session_id).where('').get().then(session =>{
+    admin.firestore().collection(day).doc(documentId).get().then(session =>{
         if (!session.exists) {
-            console.log('No such session')
+            console.log('No such session');
         }else{
             var eventName= session.data().title;
             var eventVenue= session.data().room;
             var eventTime= session.data().time;
 
-            console.log('eventName', eventName);
-            console.log('eventVenue', eventVenue);
-            console.log('eventTime', eventTime);
+            console.log('eventName is ' + eventName);
+            console.log('eventVenue is ' +eventVenue);
+            console.log('eventTime is ' +eventTime);
         }
+    })
+    .catch(err => {
+        console.log('Error getting session',err);
     });
     
-
-
-
-    return true;
+return true;
 
 
 });
