1a9a49b5c86ca04dc1f9c9342117ad86c7e61f4a
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-16 23:56:59 +0300

    refactor: Refactor DayOne feature
    This commit refactors the DayOne feature to  make use of coroutines for network calls and database writes

--- app/src/main/java/droiddevelopers254/droidconke/di/modules.kt
+++ app/src/main/java/droiddevelopers254/droidconke/di/modules.kt
@@ -1,9 +1,12 @@
 package droiddevelopers254.droidconke.di
 
+import androidx.room.Room
 import com.google.firebase.firestore.FirebaseFirestore
+import droiddevelopers254.droidconke.database.AppDatabase
 import droiddevelopers254.droidconke.repository.AboutDetailsRepo
-import droiddevelopers254.droidconke.repository.EventTypeRepo
+import droiddevelopers254.droidconke.repository.DayOneRepo
 import droiddevelopers254.droidconke.viewmodels.AboutDetailsViewModel
+import droiddevelopers254.droidconke.viewmodels.DayOneViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -13,9 +16,14 @@ val appModule = module {
 
     // ViewModels
     viewModel { AboutDetailsViewModel(get()) }
+    viewModel { DayOneViewModel(get()) }
 }
 
 val dataModule = module {
+    // Repos
     single { AboutDetailsRepo(get()) }
-    single { EventTypeRepo() }
+    single { DayOneRepo(get(), get()) }
+
+    // Database
+    single { Room.databaseBuilder(get(), AppDatabase::class.java, "droidconKE_db").build() }
 }
--- app/src/main/java/droiddevelopers254/droidconke/views/fragments/DayOneFragment.kt
+++ app/src/main/java/droiddevelopers254/droidconke/views/fragments/DayOneFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +20,7 @@ import droiddevelopers254.droidconke.views.activities.SessionViewActivity
 import kotlinx.android.synthetic.main.fragment_day_one.*
 import kotlinx.android.synthetic.main.fragment_day_one.view.*
 import org.jetbrains.anko.toast
+import org.koin.android.ext.android.inject
 import java.util.*
 
 class DayOneFragment : Fragment() {
@@ -28,14 +28,14 @@ class DayOneFragment : Fragment() {
     internal var sessionsModelList: List<SessionsModel> = ArrayList()
     internal var sessionTimeModelList: List<SessionTimeModel> = ArrayList()
     internal var sessionIds: List<String> = ArrayList()
-    lateinit var dayOneViewModel: DayOneViewModel
     internal var isStarred: Boolean = false
     lateinit var sessionsAdapter: SessionsAdapter
+
+    private val dayOneViewModel: DayOneViewModel by inject()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_day_one, container, false)
 
-        dayOneViewModel = ViewModelProviders.of(this).get(DayOneViewModel::class.java)
         sessionsAdapter = SessionsAdapter(activity!!, sessionsModelList, "day_one")
         val sessionsRv = view.sessionsRv
 
