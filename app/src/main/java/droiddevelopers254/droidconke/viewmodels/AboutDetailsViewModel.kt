6f19d9b4392e111713c8c429e2c129f45d6457fa
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-16 22:23:59 +0300

    feat: Add a BaseViewModel

--- app/src/main/java/droiddevelopers254/droidconke/viewmodels/AboutDetailsViewModel.kt
+++ app/src/main/java/droiddevelopers254/droidconke/viewmodels/AboutDetailsViewModel.kt
@@ -2,20 +2,18 @@ package droiddevelopers254.droidconke.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import droiddevelopers254.droidconke.datastates.AboutDetailsState
 import droiddevelopers254.droidconke.repository.AboutDetailsRepo
 import droiddevelopers254.droidconke.utils.launchIdling
-import kotlinx.coroutines.GlobalScope
 
-class AboutDetailsViewModel(private val aboutDetailsRepo: AboutDetailsRepo) : ViewModel() {
+class AboutDetailsViewModel(private val aboutDetailsRepo: AboutDetailsRepo) : BaseViewModel() {
     private val detailsStateMediatorLiveData: MutableLiveData<AboutDetailsState> = MutableLiveData()
 
     val aboutDetails: LiveData<AboutDetailsState>
         get() = detailsStateMediatorLiveData
 
     fun fetchAboutDetails(aboutType: String) {
-        GlobalScope.launchIdling {
+        launchIdling {
             val state = aboutDetailsRepo.getAboutDetails(aboutType)
             detailsStateMediatorLiveData.postValue(state)
         }
