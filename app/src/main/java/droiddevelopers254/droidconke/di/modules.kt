1b5fda1c4d778ca85dca597026f89b91c57aa732
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-17 00:06:23 +0300

    refactor: Refactor DayTwo feature
    This commit refactors the DayTwo feature to  make use of coroutines for network calls and database writes

--- app/src/main/java/droiddevelopers254/droidconke/di/modules.kt
+++ app/src/main/java/droiddevelopers254/droidconke/di/modules.kt
@@ -5,8 +5,10 @@ import com.google.firebase.firestore.FirebaseFirestore
 import droiddevelopers254.droidconke.database.AppDatabase
 import droiddevelopers254.droidconke.repository.AboutDetailsRepo
 import droiddevelopers254.droidconke.repository.DayOneRepo
+import droiddevelopers254.droidconke.repository.DayTwoRepo
 import droiddevelopers254.droidconke.viewmodels.AboutDetailsViewModel
 import droiddevelopers254.droidconke.viewmodels.DayOneViewModel
+import droiddevelopers254.droidconke.viewmodels.DayTwoViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -17,12 +19,14 @@ val appModule = module {
     // ViewModels
     viewModel { AboutDetailsViewModel(get()) }
     viewModel { DayOneViewModel(get()) }
+    viewModel { DayTwoViewModel(get()) }
 }
 
 val dataModule = module {
     // Repos
     single { AboutDetailsRepo(get()) }
     single { DayOneRepo(get(), get()) }
+    single { DayTwoRepo(get(), get()) }
 
     // Database
     single { Room.databaseBuilder(get(), AppDatabase::class.java, "droidconKE_db").build() }
--- app/src/main/java/droiddevelopers254/droidconke/viewmodels/DayTwoViewModel.kt
+++ app/src/main/java/droiddevelopers254/droidconke/viewmodels/DayTwoViewModel.kt
@@ -2,27 +2,26 @@ package droiddevelopers254.droidconke.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.ViewModel
 import droiddevelopers254.droidconke.datastates.SessionsState
 import droiddevelopers254.droidconke.repository.DayTwoRepo
-import droiddevelopers254.droidconke.repository.RoomStarrSessionRepo
+import droiddevelopers254.droidconke.utils.launchIdling
 
-class DayTwoViewModel : ViewModel() {
+class DayTwoViewModel(private val dayTwoRepo: DayTwoRepo) : BaseViewModel() {
     private val sessionsStateMediatorLiveData: MediatorLiveData<SessionsState> = MediatorLiveData()
-    private val dayTwoRepo: DayTwoRepo = DayTwoRepo()
-    private val roomStarrSessionRepo: RoomStarrSessionRepo = RoomStarrSessionRepo()
+//    private val roomStarrSessionRepo: RoomStarrSessionRepo = RoomStarrSessionRepo()
 
     val sessions: LiveData<SessionsState>
         get() = sessionsStateMediatorLiveData
 
     fun getDayTwoSessions() {
-        val sessionsStateLiveData = dayTwoRepo.dayTwoSessions
-        sessionsStateMediatorLiveData.addSource(sessionsStateLiveData
-        ) { sessionsStateMediatorLiveData ->
-            when {
-                this.sessionsStateMediatorLiveData.hasActiveObservers() -> this.sessionsStateMediatorLiveData.removeSource(sessionsStateLiveData)
+        launchIdling {
+            val sessionsStateLiveData = dayTwoRepo.getSessions()
+            sessionsStateMediatorLiveData.addSource(sessionsStateLiveData) {
+                when {
+                    sessionsStateLiveData.hasActiveObservers() -> sessionsStateMediatorLiveData.removeSource(sessionsStateLiveData)
+                }
+                sessionsStateMediatorLiveData.value = it
             }
-            this.sessionsStateMediatorLiveData.setValue(sessionsStateMediatorLiveData)
         }
     }
 
