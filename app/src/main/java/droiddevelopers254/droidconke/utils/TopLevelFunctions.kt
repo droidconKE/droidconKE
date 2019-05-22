5e7fac59bfa896c4b164b79126c65ed7ce25bad8
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-22 16:59:17 +0300

    fix: Correct issues with constraint layout
    Changes:
    - Upgrade gradle build tools
    - Downgrade to a stable version of constraint layout

--- app/src/main/java/droiddevelopers254/droidconke/utils/TopLevelFunctions.kt
+++ app/src/main/java/droiddevelopers254/droidconke/utils/TopLevelFunctions.kt
@@ -1,10 +1,9 @@
 package droiddevelopers254.droidconke.utils
 
-import java.io.IOException
-
-suspend fun <T : Any> safeFirebaseCall(call: suspend () -> Result<T>, errorMessage: String): Result<T> = try {
-    call.invoke()
-} catch (e: Exception) {
-    Result.Error(IOException(errorMessage, e))
-}
-val <T> T.exhaustive: T get() = this
+// TODO Work in progress
+//suspend fun <T : Any> safeFirebaseCall(call: suspend () -> Result<T>, errorMessage: String): Result<T> = try {
+//    call.invoke()
+//} catch (e: Exception) {
+//    Result.Error(IOException(errorMessage, e))
+//}
+//val <T> T.exhaustive: T get() = this
