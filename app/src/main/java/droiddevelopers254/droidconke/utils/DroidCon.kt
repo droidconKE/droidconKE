4cf92e83895255c9265c96838e41d8087902d91b
Harun Wangereka <harunwangereka@gmail.com>
2020-09-30 15:49:11 +0300

    Updates

--- app/src/main/java/droiddevelopers254/droidconke/utils/DroidCon.kt
+++ app/src/main/java/droiddevelopers254/droidconke/utils/DroidCon.kt
@@ -9,12 +9,13 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.EmptyLogger
+import org.koin.core.logger.Level
 
 class DroidCon : Application() {
   override fun onCreate() {
     super.onCreate()
     startKoin {
-      if (BuildConfig.DEBUG) androidLogger() else EmptyLogger()
+      if (BuildConfig.DEBUG) androidLogger(Level.NONE)
       androidContext(this@DroidCon)
       modules(appModule, dataModule)
     }
